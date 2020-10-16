d38c58d56dbf2d3eb49681feb07f61c1d9373bd7
Muhammad Zuhaily <2019705303@isiswa.uitm.edu.my>
2020-10-16 22:49:10 +0800

    define all routes for CategoriesController and make a create page for creating new category

---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="d-flex justify-content-end mb-2">
+        <a href="{{ route('categories.create') }}" class="btn btn-success">Add Category</a> <!-- here, use route or name of the route instead of /category/create because that url may want to change later. by useing route or route name, no need to change at other place if the url is changed  -->
+    </div>
+
+    <div class="card card-default">
+        <div class="card-header">Categories</div>
+    </div>
+
+@endsection
