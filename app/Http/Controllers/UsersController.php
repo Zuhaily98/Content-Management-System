9e612dbf7092c144d54f6e1a9eb84153a8796ca4
Muhammad Zuhaily <2019705303@isiswa.uitm.edu.my>
2020-11-04 21:17:14 +0800

    successfully created make admin function to promote normal user into an admin

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,4 +11,15 @@ class UsersController extends Controller
     {
         return view('users.index')->with('users', User::all());
     }
+    
+    public function makeAdmin(User $user)
+    {
+        $user->role = 'admin';
+
+        $user->save();
+
+        session()->flash('success', 'User successfully promoted as an Admin!');
+
+        return redirect(route('users.index'));
+    }
 }
